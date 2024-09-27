09761ce9dd13ec8777dda0ac591fdd1bc0d67775
faildede <justworkur@mail.ru>
2024-09-28 00:50:43 +0500

    some thing

--- src/components/beaty/Header.vue
+++ src/components/beaty/Header.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="container mx-auto my-6 font-montserrat flex justify-between border-b">
         <router-link to="/">
-                <div class="relative my-2">
-                        <img class="my-2 animate-pulse" src="../../assets/icon.svg" alt="">
-                </div>
+            <div class="relative my-2">
+                <img class="my-2 animate-pulse" src="../../assets/icon.svg" alt="">
+            </div>
         </router-link>
-        <div class="hidden sm:flex my-4  justify-between">
+        <div class="hidden sm:flex my-4 justify-between">
             <router-link class="" to="/about">
-                <div class="p-2 hover:border-b  border-red-600 transition ease-linear duration-300 overflow-auto">
+                <div class="p-2 hover:border-b border-red-600 transition ease-linear duration-300 overflow-auto">
                     <p class="">О нас</p>
                 </div>
             </router-link>
@@ -21,9 +21,7 @@
                     <p class="">Новости</p>
                 </div>
             </router-link>
-     
-            <div class="cursor-pointer z-50" v-if="!isAuth" @click="toggleVisible">
-           
+            <div class="cursor-pointer z-50" v-if="!isAuth" @click="visible = !visible">
                 <div class="text-white mx-4 bg-red-600 rounded-sm">
                     <p class="p-2">Создать свой профиль</p>
                 </div>
@@ -36,71 +34,64 @@
                     </router-link>
                 </div>
             </div>
-
             <ProfileLink v-else class="my-auto mx-4" />
         </div>
         <button @click="visible = !visible" class="sticky my-auto mx-4 sm:hidden cursor-pointer">
-    <svg v-if="!visible" class="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-    </svg>
-    <svg v-else class="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-    </svg>
-</button>
-
-<transition name="fade" appear>
-    <div v-if="visible"  class="absolute z-50 mt-20 w-full bg-white shadow-md rounded-lg p-4 transition-all duration-500 ease-in-out" >
-
-        <div class="container w-1/2 mx-auto  ">
-            <router-link class="" to="/about">
-                <div class="p-2 mt-2 border-b  border-red-600 transition ease-linear duration-300 overflow-auto ">
-                    <p class="">О нас</p>
-                </div>
-            </router-link>
-            <router-link class="" to="/allpost">
-                <div class="p-2 mt-2 border-b border-red-600  overflow-auto ">
-                    <p class="">Проекты</p>
-                </div>
-            </router-link>
-            <router-link class="" to="/new">
-                <div class="p-2 mt-2 border-b border-red-600  overflow-auto ">
-                    <p class="">Новости</p>
-                </div>
-            </router-link>
+            <svg v-if="!visible" class="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+            </svg>
+            <svg v-else class="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+            </svg>
+        </button>
+    </div>
+    <transition name="fade" appear>
+        <div v-if="visible" class="absolute z-50 w-full bg-white shadow-md rounded-lg p-4 transition-all duration-500 ease-in-out sm:hidden">
+            <div class="container w-1/2 mx-auto">
+                <router-link class="" to="/about">
+                    <div class="p-2 mt-2 border-b border-red-600 transition ease-linear duration-300 overflow-auto">
+                        <p class="">О нас</p>
+                    </div>
+                </router-link>
+                <router-link class="" to="/allpost">
+                    <div class="p-2 mt-2 border-b border-red-600 overflow-auto">
+                        <p class="">Проекты</p>
+                    </div>
+                </router-link>
+                <router-link class="" to="/new">
+                    <div class="p-2 mt-2 border-b border-red-600 overflow-auto">
+                        <p class="">Новости</p>
+                    </div>
+                </router-link>
                 <div v-if="!isAuth">
                     <router-link class="" to="/registration">
-                        <div class="p-2 mt-2 border-b border-red-600  overflow-auto ">
+                        <div class="p-2 mt-2 border-b border-red-600 overflow-auto">
                             <p class="">Создать свой профиль</p>
                         </div>
-                            
-                        </router-link>
-                        <router-link class="" to="/Auth">
-                            <div class="p-2 mt-2 border-b border-red-600  overflow-auto ">
-                                <p class="">Войти</p>    
-                            </div>
-                        </router-link>
-                </div>
-            
-                <div v-else>
-                    <router-link :to="`/profile/${username}`">
-                            <div class="p-2 pb-4 mt-2 border-b border-red-600  overflow-auto ">
-                                <p>Профиль</p>
-                            </div>    
+                    </router-link>
+                    <router-link class="" to="/Auth">
+                        <div class="p-2 mt-2 border-b border-red-600 overflow-auto">
+                            <p class="">Войти</p>
+                        </div>
                     </router-link>
                 </div>
-            
+                <div v-else>
+                    <router-link :to="`/profile/${profile}`">
+                        <div class="p-2 pb-4 mt-2 border-b border-red-600 overflow-auto">
+                            <p>Профиль</p>
+                        </div>
+                    </router-link>
+                </div>
+            </div>
         </div>
-        
-    </div>
-</transition>
-</div>
+    </transition>
 </template>
 
 <script>
 import ProfileLink from './ProfileLink.vue'
-import { useStore }  from '../../store/store'
-import { computed } from 'vue'
-import { ref } from 'vue'
+import { useStore } from '../../store/store'
+import { computed, ref } from 'vue'
+
 export default {
     name: 'Header',
     components: {
@@ -110,8 +101,7 @@ export default {
         const store = useStore();
         const isAuth = computed(() => store.isAuth);
         const visible = ref(false);
-        const profile = computed (() => localStorage.getItem('username'));
-
+        const profile = computed(() => localStorage.getItem('username'));
 
         return {
             isAuth,
@@ -121,7 +111,6 @@ export default {
     }
 }
 </script>
-
 
 <style scoped>
 .logout-button {
